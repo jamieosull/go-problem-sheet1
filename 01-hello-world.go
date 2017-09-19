767e5fd85e570175391c3971057eabfd305a49a3
Jamie O Sullivan <Jamie O Sullivan>
2017-09-19 14:52:08 +0100

    My first commit

---
+++ 01-hello-world.go
@@ -0,0 +1,11 @@
+// Adapted from https://tour.golang.org/welcome/1s
+//
+// Hello, world! In Go
+// Author: Jamie O'Sullivan;
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, ??")
+}
