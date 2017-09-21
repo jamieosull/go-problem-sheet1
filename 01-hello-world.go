679a8d4329a79a50daff4d1f317db8f7a0dedffd
Jamie O Sullivan <Jamie O Sullivan>
2017-09-21 19:48:51 +0100

    Change to characters

--- 01-hello-world.go
+++ 01-hello-world.go
@@ -1,11 +1,12 @@
 // Adapted from https://tour.golang.org/welcome/1s
 //
-// Hello, world! In Go
+// 
 // Author: Jamie O'Sullivan;
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, ??")
+	fmt.Println("「こんにちは世界」")
+
 }
