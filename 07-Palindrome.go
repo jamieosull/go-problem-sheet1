0c1cfd5d7d6b2faf4cd8ce09ca7527e9e05f1b04
Jamie O Sullivan <Jamie O Sullivan>
2017-09-29 12:37:46 +0100

    Plindrome

---
+++ 07-Palindrome.go
@@ -0,0 +1,33 @@
+// Adapted fromhttp://golangcookbook.blogspot.ie/2012/11/guess-number-game-in-golang.html
+//
+//
+// Author: Jamie O'Sullivan;
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var ip string
+	var ip string
+	fmt.Println("Enter string:")
+	fmt.Scanf("%s\n", &ip)
+	ip = strings.ToLower(ip)
+	fmt.Println(isP(ip))
+}
+
+//Function to test if the string entered is a Palindrome
+
+func isP(s string) string {
+	mid := len(s) / 2
+	last := len(s) - 1
+	for i := 0; i < mid; i++ {
+		if s[i] != s[last-i] {
+			return "NO. It's not a Palimdrome."
+		}
+	}
+	return "YES! You've entered a Palindrome"
+
+}
