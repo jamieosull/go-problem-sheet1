705d04952d318b73f90941935a56d0d1a326207e
Jamie O Sullivan <Jamie O Sullivan>
2017-10-05 17:09:31 +0100

    Q8

---
+++ 08-MergeListAnSort.go
@@ -0,0 +1,61 @@
+//Adpoted from http://austingwalters.com/merge-sort-in-go-golang/
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	slice := generateSlice(50)
+	fmt.Println("\n --- unsorted --- \n\n", slice)
+	fmt.Println("\n--- sorted ---\n\n", MergeSort(slice), "\n")
+}
+
+// Generates a slice of size, size filled with random numbers
+func generateSlice(size int) []int {
+
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(99999) - rand.Intn(99999)
+	}
+	return slice
+}
+
+// Runs MergeSort algorithm on a slice single
+func MergeSort(slice []int) []int {
+
+	if len(slice) < 2 {
+		return slice
+	}
+	mid := (len(slice)) / 2
+	return Merge(MergeSort(slice[:mid]), MergeSort(slice[mid:]))
+}
+
+// Merges left and right slice into newly created slice
+func Merge(left, right []int) []int {
+
+	size, i, j := len(left)+len(right), 0, 0
+	slice := make([]int, size, size)
+
+	for k := 0; k < size; k++ {
+		if i > len(left)-1 && j <= len(right)-1 {
+			slice[k] = right[j]
+			j++
+		} else if j > len(right)-1 && i <= len(left)-1 {
+			slice[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			slice[k] = left[i]
+			i++
+		} else {
+			slice[k] = right[j]
+			j++
+		}
+	}
+	return slice
+}
