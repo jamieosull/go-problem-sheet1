84b1350b2cc8275e6894be51a945820c82c4045d
Jamie O Sullivan <Jamie O Sullivan>
2017-10-06 14:24:39 +0100

    Q9

--- 09-Newton.go
+++ 09-Newton.go
@@ -1,4 +1,4 @@
-// Adapted from https://tour.golang.org/welcome/1s
+// Adapted from https://play.golang.org/p/J4gxs46HET
 //
 //
 // Author: Jamie O'Sullivan;
@@ -9,7 +9,7 @@ import (
 	"math"
 )
 
-func z_next(z float64, x float64) float64 {
+func znext(z float64, x float64) float64 {
 
 	return z - (((z * z) - x) / (2 * z))
 }
@@ -21,7 +21,7 @@ func main() {
 	//The Guess
 	z := float64(1)
 
-	for z = 1.0; z != z_next(z, x); z = z_next(z, x) {
+	for z = 1.0; z != znext(z, x); z = znext(z, x) {
 		fmt.Println("Current Guess:&f \n", z)
 
 	}
