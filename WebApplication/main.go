f38a5bb0c42d50922a7daeb93f8abd3a4e42fd8a
Jamie O Sullivan <Jamie O Sullivan>
2017-11-11 12:45:37 +0000

    bot

---
+++ WebApplication/main.go
@@ -0,0 +1,29 @@
+// author: Jamie O'Sullivan
+// ChatBot
+//
+
+package main
+
+// These are the libraries we are going to use
+// Both "fmt" and "net" are part of the Go standard library
+import (
+	// "fmt" has methods for formatted I/O operations (like printing to the console)
+	"fmt"
+	// The "net/http" library has methods to implement HTTP clients and servers
+	"net/http"
+)
+
+func main() {
+	// The "HandleFunc" method accepts a path and a function as arguments
+	http.HandleFunc("/", handler)
+
+	// After defining the server, "listen and serve" on port 8080
+	http.ListenAndServe(":8080", nil)
+}
+
+// It has to follow the function signature of a ResponseWriter and Request type
+// as the arguments.
+func handler(w http.ResponseWriter, r *http.Request) {
+	// For this case, we will always pipe "Hello World" into the response writer
+	fmt.Fprintf(w, "Hello World!")
+}
